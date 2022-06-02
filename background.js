05febdb82a96b5b04b359657f9f0c96247fd7410
Adrien Didot <52965871+Adridot@users.noreply.github.com>
2022-06-03 01:32:56 +0300

    Popup - Front-end displaying inputs & outputs - Back-end:   - functions to connect to the api   - functions to provide the right data to the api

--- background.js
+++ background.js
@@ -14,24 +14,55 @@ let address_list = [
         name: "Travail SG",
         address: "Tours Société Générale La Défense",
     },
-    ];
+];
+
+let distances = {
+    // origin: "",
+    origin: "37 rue Rémy Dumoncel 77210 Avon",
+    destinations: [
+        {
+            name: "Maison",
+            travel_time: "1 hour 30 minutes",
+        },
+        {
+            name: "Ecole",
+            travel_time: "45 minutes",
+        },
+        {
+            name: "Travail SG",
+            travel_time: "37 minutes",
+        },
+        {
+            name: "Travail SG",
+            travel_time: "37 minutes",
+        },
+    ],
+    //destinations: {name:"", travel_time:""},
+}
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({address_list});
+    chrome.storage.sync.set({distances});
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.name === "autocomplete") {
+    if (request.name === "distance") {
         const apiKey = "YOUR-API-KEY-HERE";
-        const url = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`
+        let params = `origin=${request.origin}&destination=${request.destination}&mode=${request.transport_mode}`
+        if (request.departure_time !== "") {
+            params += `&departure_time=${request.departure_time}`
+        }
+        const url = `https://maps.googleapis.com/maps/api/directions/json?${params}&key=${apiKey}`;
 
         fetch(url).then(function (response) {
+            console.log(response);
             if (response.status !== 200) {
                 console.log(`Error: ${response.status}`);
                 return;
             }
-            response.text().then(function (text) {
-                sendResponse(text);
+            response.json().then(function (data) {
+                console.log(data);
+                sendResponse(data);
             });
         })
     }
--- popup.js
+++ popup.js
@@ -1,23 +1,114 @@
-let changeColor = document.getElementById("changeColor");
+window.onload = function () {
+    updateTable()
+}
 
-chrome.storage.sync.get("color", ({color}) => {
-    changeColor.style.backgroundColor = color;
-});
+function clearTable() {
+    document.getElementById("results_table_body").innerHTML = "";
+}
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+function clearDestinations() {
+    chrome.storage.sync.set({distances: {origin: "", destinations: []}});
+    updateTable();
+}
 
-    await chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        function: setPageBackgroundColor,
+function getDate(hours, minutes) {
+    let today = new Date();
+    today.setHours(hours);
+    today.setMinutes(minutes);
+
+    return Math.round(today.getTime()/1000);
+}
+
+function distancesToHTML(distances) {
+    clearTable();
+    for (let destination of distances.destinations) {
+        let row = document.createElement("tr");
+        let name_cell = document.createElement("td");
+        let travel_time_cell = document.createElement("td");
+        name_cell.innerHTML = destination.name;
+        travel_time_cell.innerHTML = destination.travel_time;
+        row.appendChild(name_cell);
+        row.appendChild(travel_time_cell);
+        document.getElementById("results_table_body").appendChild(row);
+    }
+}
+
+function updateTable() {
+    chrome.storage.sync.get("distances", ({distances}) => {
+        if (distances.origin === "") {
+            document.getElementById("results_container").classList.add('is-hidden');
+        } else {
+            document.getElementById("results_container").classList.remove('is-hidden');
+            document.getElementById("current_origin_address").innerHTML = `The selected address is: <b>${distances.origin}</b>`;
+            distancesToHTML(distances);
+        }
     });
+}
+
+document.getElementById("transport_mode_select").addEventListener("change", () => {
+    let transport_mode = document.getElementById("transport_mode_select").value;
+    if (transport_mode === "transit") {
+        document.getElementById("departure_time_container").classList.remove('is-hidden');
+    } else {
+        document.getElementById("departure_time_container").classList.add('is-hidden');
+    }
 });
 
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({color}) => {
-        document.body.style.backgroundColor = color;
+document.getElementById("setup_button").addEventListener("click", () => {
+    chrome.runtime.openOptionsPage();
+});
+
+document.getElementById("address_button").addEventListener("click", () => {
+    let address_input = document.getElementById("address");
+    let address = address_input.value;
+    if (address === "") {
+        alert("Please fill in an address");
+        return;
+    }
+    let transport_mode = document.getElementById("transport_mode_select").value;
+    let departure_time = ""
+    if (transport_mode === "transit") {
+        let hour = document.getElementById("departure_hour_select").value;
+        let minutes = document.getElementById("departure_minutes_select").value;
+        departure_time = getDate(hour, minutes);
+    }
+    getTravelTimes(address, transport_mode, departure_time);
+});
+
+function getTravelTimes(origin, transport_mode, departure_time) {
+    let destinations = []
+    chrome.storage.sync.get("address_list", ({address_list}) => {
+        for (let address_elem of address_list) {
+            console.log(origin, address_elem.address, transport_mode, departure_time)
+            let travel_time = extractTravelTime(origin, address_elem.address, transport_mode, departure_time);
+            destinations.push({name: address_elem.name, travel_time: travel_time});
+        }
+    });
+    chrome.storage.sync.get("distances", ({distances}) => {
+        distances.origin = origin;
+        distances.destinations = destinations;
+        chrome.storage.sync.set({distances: distances});
+    });
+    updateTable();
+}
+
+async function extractTravelTime(origin, destination, transport_mode, departure_time) {
+    const apiResponse = await callAPI(origin, destination, transport_mode, departure_time);
+    console.log(apiResponse);
+    return apiResponse.routes[0].legs[0].duration.text;
+}
+
+
+function callAPI(origin, destination, transport_mode, departure_time) {
+    message = {
+        name: "distance",
+        origin: encodeURIComponent(origin),
+        destination: encodeURIComponent(destination),
+        transport_mode: encodeURIComponent(transport_mode),
+        departure_time: encodeURIComponent(departure_time)
+    }
+
+    chrome.runtime.sendMessage(message, (response) => {
+        return response;
     });
 }
